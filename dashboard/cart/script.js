987f0aa2e83b3d4a02b36b92e410da781f118397
Ivan Negohe <inegohe@student.refactory.academy>
2024-03-27 15:24:17 +0300

    did soe theming

--- dashboard/cart/script.js
+++ dashboard/cart/script.js
@@ -22,7 +22,7 @@ function item(array) {
     if(array != null){
         for (i = 0; i < array.length; i++) {
             document.getElementById("cart-items").insertAdjacentHTML("afterend", `<li>${array[i].title}</li>`);
-            document.getElementById("feed").insertAdjacentHTML("afterend", `<div class="col-8">
+            document.getElementById("feed").insertAdjacentHTML("afterend", `<div class="col">
             <div class="card rounded shadow border-0 mb-4">
                 <div class="row g-0">
                     <div class="col-md-4"><img class="img-fluid rounded-start"
@@ -32,7 +32,7 @@ function item(array) {
                             <h4 class="card-title">${array[i].title}</h4>
                             <h5 class="card-title"></h5>
                             <p class="card-text">Ush ${array[i].amount}</p>
-                            <a href="#" class="btn btn-primary">BUY NOW</a></div>
+                            <a href="#" class="btn btn-danger">BUY NOW</a></div>
                     </div>
                 </div>
             </div>
@@ -41,7 +41,7 @@ function item(array) {
     }else{
         document.getElementById("feed").innerHTML = "<h2>Your Cart is Empty....</h2>";
     }
-    
+    total(array);
 }
 
 function clearCart() {
